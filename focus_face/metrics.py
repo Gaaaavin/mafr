9f6eac942f54516fb32d25e83e50184e1074d493
Xinhao Liu <gavinliu_hm@hotmail.com>
2022-11-15 02:36:06 -0500

    format metrics

--- focus_face/metrics.py
+++ focus_face/metrics.py
@@ -3,8 +3,7 @@ from pyeer.report import generate_eer_report, export_error_rates
 from pyeer.plot import plot_eer_stats
 
 
-
-def calculate_metrics(gen_scores,fake_scores,epoch):
+def calculate_metrics(gen_scores, fake_scores, epoch):
     metrics = get_eer_stats(gen_scores, fake_scores)
     generate_eer_report([metrics], ['A'], 'pyeer_report_'+str(epoch)+'.html')
-    return metrics.fmr0,metrics.fmr100,metrics.fmr1000,metrics.gmean,metrics.imean,metrics.auc
+    return metrics.fmr0, metrics.fmr100, metrics.fmr1000, metrics.gmean, metrics.imean, metrics.auc
