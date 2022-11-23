360b666cdf9eab486c1a6fc617f4815f0baf4ddc
Eddy Shao <eddyszh@outlook.com>
2022-11-22 22:46:19 -0500

    update new baseline

---
+++ ArcFace/dataset.py
@@ -0,0 +1,92 @@
+import sys
+sys.path.append("../")
+
+import os
+from tqdm import tqdm
+
+import numpy as np
+import torch
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+
+from utils import load_n_add_mask
+
+
+np.random.seed(42)
+torch.manual_seed(42)
+
+
+class TrainDataset(ImageFolder):
+    def __init__(self, root: str, msk_args, transform=None):
+        super().__init__(root, transform)
+        self.msk_args = msk_args
+        if transform is None:
+            # Deafault transform
+            self.transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.RandomHorizontalFlip(),
+                transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
+            ])
+
+    def __getitem__(self, index: int):
+        path, id_target = self.samples[index]
+        img_msk, img_raw, success = load_n_add_mask(path, self.msk_args)
+        img_msk = self.transform(img_msk)
+        img_raw = self.transform(img_raw)
+        return {"masked image": img_msk, "raw image": img_raw, "identity": id_target, "mask": success}
+
+
+class EvalDataset(ImageFolder):
+    def __init__(self, root: str, msk_args, prob_pos=0.5, prob_msk=0.5, transform=None):
+        super().__init__(root, transform)
+        self.msk_args = msk_args
+        self.classes = os.listdir(root)
+        self.msk_prob=prob_msk
+        if transform is None:
+            # Deafault transform
+            self.transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+            ])
+
+        # Generate list
+        self.img_path = []
+        self.label_same = []
+        self.label_mask = []
+        for i in range(len(self)):
+            anchor_path, anchor_id = self.samples[i]
+            j = anchor_id
+            if np.random.uniform(0, 1) < prob_pos:
+                self.label_same.append(1)
+            else:
+                self.label_same.append(0)
+                while j == anchor_id:
+                    j = np.random.choice(len(self))
+            choices = os.listdir(os.path.join(root, self.classes[j]))
+            other_path = os.path.join(root, self.classes[j], choices[np.random.choice(len(choices))])
+
+            self.label_mask.append(np.random.uniform(0, 1, size=2)) # seperate for anchor and other
+           
+            self.img_path.append((anchor_path, other_path))
+
+
+    def __len__(self):
+        return len(self.classes)
+
+
+    def __getitem__(self, index):
+        anchor_path, other_path = self.img_path[index]
+        if self.label_mask[index][0] < self.msk_prob:
+            img_anchor, _, _ = load_n_add_mask(anchor_path, self.msk_args)
+        else:
+            img_anchor = self.loader(anchor_path)
+
+        if self.label_mask[index][1] < self.msk_prob:
+            img_other, _, _ = load_n_add_mask(other_path, self.msk_args)
+        else:
+            img_other = self.loader(anchor_path)
+
+        img_anchor = self.transform(img_anchor)
+        img_other = self.transform(img_other)
+
+        return {"anchor": img_anchor, "other": img_other, "same": self.label_same[index]}
---
+++ script/train_arcface.py
@@ -0,0 +1,221 @@
+import sys
+sys.path.insert(0, '../')
+
+from tqdm import tqdm
+import multiprocessing
+import os
+import time
+from argparse import ArgumentParser, Namespace
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from pyeer.eer_info import get_eer_stats
+from torchmetrics import Accuracy
+
+from ArcFace.resnet import *
+from ArcFace.model import *
+from ArcFace.dataset import TrainDataset, EvalDataset
+import utils
+
+
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = True
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+parser = ArgumentParser()
+parser.add_argument('--name', type=str, default='test', help='experiment name')
+parser.add_argument('-E', '--n_epochs', type=int,
+                    default=500, help='number of epochs')
+parser.add_argument('--lr', type=float, default=1e-4, help='learning rate')
+parser.add_argument('-t', '--train', type=str,
+                    default="/WebFace1k", help='training dataset')
+parser.add_argument('-e', '--eval', type=str, default=None,
+                    help='evaluation dataset')
+parser.add_argument('-B', "--bs", type=int, default=480, help="Batch size")
+parser.add_argument('--log_interval', type=int, default=10,
+                    help='logging interval of saving results')
+parser.add_argument('--resume', default=False, action='store_true',
+                    help='toggle resuming')
+parser.add_argument('--amp', default=False, action='store_true',
+                    help="Toggle auto mixed precision")
+parser.add_argument("-v", "--verbose", default=False, action="store_true", help="Display tqdm bar")
+parser.add_argument('--backbone', default='resnet18', help='backbone network type')
+parser.add_argument('--pretrained', default=True, action='store_true', help='if pretrained resnet')
+parser.add_argument('--metric', default='add_margin', help='embedding head of model')
+opt = parser.parse_args()
+
+
+checkpoint_dir = os.path.join('../res',opt.name)
+if not os.path.exists(checkpoint_dir):
+    os.mkdir(checkpoint_dir)
+
+
+print(opt.pretrained)
+
+mask_args = Namespace()
+mask_args.mask_type = "random"
+mask_args.detector, mask_args.predictor = utils.msk_init()
+mask_args.verbose = False
+mask_args.code, mask_args.pattern, mask_args.color = "", "", ""
+
+
+workers = min(8, multiprocessing.cpu_count())
+
+
+print("Loading dataset")
+train_dataset = TrainDataset(opt.train, mask_args)
+eval_dataset = EvalDataset(opt.eval, mask_args)
+train_loader = DataLoader(train_dataset, batch_size=opt.bs, num_workers=workers, shuffle=True, pin_memory=True)
+eval_loader = DataLoader(eval_dataset, batch_size=opt.bs, num_workers=workers, shuffle=False, pin_memory=True)
+identities = len(train_dataset.classes)
+
+
+print("Creating model")
+
+if opt.backbone == 'resnet18':
+    model = resnet_face18(use_se=True).to(device)
+elif opt.backbone == 'resnet34':
+    model = resnet34().to(device)
+elif opt.backbone == 'resnet50':
+    model = resnet50().to(device)
+
+if opt.metric == 'add_margin':
+    metric_fc = AddMarginProduct(512, identities, s=30, m=0.35).to(device)
+elif opt.metric == 'arc_margin':
+    metric_fc = ArcMarginProduct(512, identities, s=30, m=0.5, easy_margin=opt.easy_margin).to(device)
+elif opt.metric == 'sphere':
+    metric_fc = SphereProduct(512, identities, m=4).to(device)
+else:
+    metric_fc = nn.Linear(512, identities).to(device)
+    
+
+optimizer = optim.Adam([{'params': model.parameters()}, {'params': metric_fc.parameters()}], lr=opt.lr)
+MSE = nn.MSELoss()
+CE = nn.CrossEntropyLoss()  
+if opt.amp:
+    scaler = torch.cuda.amp.GradScaler()
+
+if opt.resume:
+    resume_filename = os.path.join(checkpoint_dir, "model_best.pth")
+    print("Resuming From ", resume_filename)
+
+    state = torch.load(resume_filename)
+    starting_epoch = state["epoch"]
+    model.load_state_dict(state["model"])
+    optimizer.load_state_dict(state["optimizer"])
+    if opt.amp:
+        scaler.load_state_dict(state["scaler"])
+else:
+    starting_epoch = 0
+
+
+print("Start training")
+best_score = 100
+training_losses = []
+for epoch in range(starting_epoch, opt.n_epochs):
+    # Train
+    epoch_loss = 0
+    model.train()
+
+    time_start = time.time()
+    print('With new loss function')
+    for sample in tqdm(train_loader, disable=not opt.verbose):
+        img_raw = sample['masked image'].to(device)
+        img_msk = sample['raw image'].to(device)
+        id = sample['identity'].to(device)
+        mask = sample['mask'].to(device)
+         
+        # print(img_msk.shape)
+        # print(img_raw.shape)
+        # print(id)
+        # print(mask)
+        
+        feature_raw = model(img_raw)
+        feature_msk = model(img_msk)
+        
+        output_raw = metric_fc(feature_raw, id)
+        output_msk = metric_fc(feature_msk, id)
+
+        # loss = CE(output_raw, id) + CE(output_raw, id)
+        print(MSE(feature_raw, feature_msk))
+        
+        loss = CE(output_raw, id) + CE(output_raw, id) + 3*MSE(feature_raw, feature_msk)
+        
+
+        if opt.amp:
+            scaler.scale(loss).backward()
+            scaler.unscale_(optimizer)
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=0.1)
+            scaler.step(optimizer)
+            scaler.update()
+            optimizer.zero_grad(set_to_none=True)
+        else:
+            optimizer.zero_grad(set_to_none=True)
+            loss.backward()
+            optimizer.step()
+
+        epoch_loss += loss.item()
+    
+    time_end = time.time()
+
+    epoch_loss /= len(train_loader)
+    training_losses.append(epoch_loss)
+    if epoch % opt.log_interval == 0:        
+        print("Training time: {:.2f}s".format(time_end - time_start))
+        print('[{}/{}], training loss: {:.4f}'.format(epoch+1, opt.n_epochs, epoch_loss))
+
+    # Evaluation
+    print("Start evaluation")
+    
+    model.eval()
+    with torch.no_grad():
+        positives = []
+        negatives = []
+        accuracy = []
+        acc_metric = Accuracy()
+        for sample in tqdm(eval_loader, disable=not opt.verbose):
+            img_anchor = sample["anchor"].to(device)
+            img_other = sample["other"].to(device)
+            label_same = sample["same"]
+
+            feature_anchor = model(img_anchor) 
+            feature_other = model(img_other) 
+            
+            feature_anchor = F.normalize(feature_anchor).unsqueeze(1)
+            feature_other = F.normalize(feature_other).unsqueeze(1)
+            
+            dist = 1 - torch.cdist(feature_anchor, feature_other) / 2
+            
+            
+            for i in range(label_same.shape[0]):
+                if label_same[i] == 1:
+                    positives.append(dist[i].squeeze().cpu().numpy())
+                else:
+                    negatives.append(dist[i].squeeze().cpu().numpy())
+
+        metrics = get_eer_stats(positives, negatives)
+        # print(metrics)
+    
+    fmr100 = metrics.fmr100
+    if epoch % opt.log_interval == 0:
+        print("FMR 100:", fmr100)
+        print("AUC:", metrics.auc)
+        print("Other metrics:metrics.fmr0, metrics.fmr100, metrics.fmr1000, metrics.gmean, metrics.imean, metrics.auc")
+        print(metrics.fmr0, metrics.fmr100, metrics.fmr1000, metrics.gmean, metrics.imean, metrics.auc
+)
+        print()
+        
+#     # Save checkpoint
+#     if fmr100 < best_score:
+#         best_score = fmr100
+#         checkpoint = {"model": model.state_dict(),
+#               "optimizer": optimizer.state_dict(),
+#               "scaler": scaler.state_dict(),
+#               "epoch": epoch
+#         }
+#         torch.save(checkpoint, os.path.join(checkpoint_dir, "model_best.pth"))
+#         print('best model saved.')
