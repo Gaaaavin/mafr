ec2b800d9755edba1523ad9e459c2246e805864c
Xinhao Liu <gavinliu_hm@hotmail.com>
2022-12-15 09:13:52 -0500

    Update plot script

--- misc/plot_res.py
+++ misc/plot_res.py
@@ -4,14 +4,15 @@ import os
 import json
 
 
-checkpoint_dirs = [
-    "../res/arcface_wf",
-    "../res/arcface_lfw",
-    "../res/arcface_dist_wf",
-    "../res/arcface_dist_lfw"
-]
+# checkpoint_dirs = [
+#     "../res/arcface_30k_p0",
+#     "../res/arcface_30k_p0_dist",
+#     "../res/arcface_30k_p5",
+#     "../res/arcface_30k_p5_dist"
+# ]
+checkpoint_dirs = os.listdir("../res")
 for checkpoint_dir in checkpoint_dirs:
-    res_path = os.path.join(checkpoint_dir, "results.json")
+    res_path = os.path.join("../res", checkpoint_dir, "results.json")
     with open(res_path, 'rt') as f:
         results = json.load(f)
 
@@ -21,5 +22,5 @@ for checkpoint_dir in checkpoint_dirs:
     plt.xlabel("Epoch")
     plt.ylabel("Accuracy")
     plt.title(checkpoint_dir.split("/")[-1])
-    plt.savefig(os.path.join(checkpoint_dir, "results.pdf"))
+    plt.savefig(os.path.join("fig", checkpoint_dir+".pdf"))
     plt.close()
